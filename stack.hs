a83511a532e4f17c8e1565f731466e84bbc6612b
Manoj Ransing <manoj.ransing@gmail.com>
2012-09-07 16:39:02 +0530

    Made changes in stack

--- stack.hs
+++ stack.hs
@@ -2,6 +2,8 @@
 
 data Stack' v = Stack' [v] Int deriving (Show)
 
+data Month = Jan | Feb | Mar | Apr | May deriving (Ord,Eq,Show) 
+ 
 push' :: (Ord v) => Stack' v -> v -> Stack' v
 push' (Stack' l m) a = if m <= length l then Stack' l m else Stack' (l ++ [a]) m
 
@@ -16,8 +18,14 @@ pop (Stack l m) = if length l == 0 then Stack l m else Stack (init l) m
 
 top :: Stack -> Maybe Int 
 top (Stack l m) = if length l == 0 then Nothing else Just $ last l
- 
-main = do 
-     print("Hello World", push (Stack [1,2,3] 4) 5, pop (Stack [1,2,3] 4), top $ pop $ push (Stack [1,2,3] 4) 5)
-     print(Stack' [1,2,3] 4, push' (push' (Stack' ["Manoj","Eknath","Abhay"] 4) "Sonali") "Lakshmi")
+
+main = do
+       let
+	st = Stack' [] 5
+	st1 = Stack' [] 5
+       let
+        st2 = push' st1 Jan	
+       print(push' st "Eknath", st2)
+       print("Hello World", push (Stack [1,2,3] 4) 5, pop (Stack [1,2,3] 4), top $ pop $ push (Stack [1,2,3] 4) 5, push' st2 Feb)
+       print(Stack' [1,2,3] 4, push' (push' (Stack' ["Manoj","Eknath","Abhay"] 4) "Sonali") "Lakshmi")
 
